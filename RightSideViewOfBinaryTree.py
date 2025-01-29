9bc17852cf1110cd87b50fe74a80a1500aaac49e
Ganesh Sandeep Kanumilli <89462870+SandeepKanumilli@users.noreply.github.com>
2025-01-29 10:08:24 -0600

    second commit

--- RightSideViewOfBinaryTree.py
+++ RightSideViewOfBinaryTree.py
@@ -6,28 +6,57 @@
 #         self.right = right
 
 
-#TC = O(N)
-#SC = O(H)
+# Inorder Traversal of Tree
+from queue import Queue
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    def rightSideView(self, root : Optional[TreeNode]) -> List[int]:
         if root == None:
             return []
-        self.result = []
-        self.dfs(root,0)
-        return self.result
+        q = Queue()
+        result = []
+        q.put(root)
+        while not q.empty():
+            size = q.qsize()
+            for i in range(size):
+                curr = q.get()
+                if i == size-1:
+                    result.append(curr.val)
+                if curr.left != None:
+                    q.put(curr)
+                if curr.right != None:
+                    q.put(curr)
+                
+        return result
     
-    def dfs(self, root: Optional[TreeNode], lvl : int) ->None:
-        if root == None:
-            return
-
-        if(lvl == len(self.result)):
-            #temp = []
-            #temp.append(root.val)
-            self.result.append(root.val)
-        else:
-            self.result[lvl] = root.val
+            
+            
         
-        self.dfs(root.left, lvl + 1)
-        self.dfs(root.right, lvl + 1)
+        
+
+
+
+#TC = O(N)
+#SC = O(H)
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         if root == None:
+#             return []
+#         self.result = []
+#         self.dfs(root,0)
+#         return self.result
+    
+#     def dfs(self, root: Optional[TreeNode], lvl : int) ->None:
+#         if root == None:
+#             return
+
+#         if(lvl == len(self.result)):
+#             #temp = []
+#             #temp.append(root.val)
+#             self.result.append(root.val)
+#         else:
+#             self.result[lvl] = root.val
+        
+#         self.dfs(root.left, lvl + 1)
+#         self.dfs(root.right, lvl + 1)
         
         
